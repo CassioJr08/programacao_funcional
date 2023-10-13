e2f4a234b182173f07c85f5a9f47291c8b6fa78a
CassioJr08 <cassiojr0108@gmail.com>
2023-10-13 15:26:53 -0300

    novas alterações

--- foundation/challenge.js
+++ foundation/challenge.js
@@ -4,21 +4,21 @@ function range(...nums){
         for(let i = 1; i <= nums[0]; i++){
             array.push(i)
         }
-        console.log(array)
+        return array
 
     }else if(nums.length == 2 && nums[0] < nums[1]){
         let array = []
         for(let i = nums[0]; i <= nums[1]; i++){
             array.push(i)
         }
-        console.log(array)
+        return array
 
     }else if(nums.length == 3 && nums[0] < nums[1]){
         let array = []
         for(let i = nums[0]; i < nums[1]; i += nums[2]){
             array.push(i)
         }
-        console.log(array)
+        return array
 
     }else if(nums.length == 2 && nums[0] > nums[1]){
         let array = []
@@ -26,19 +26,19 @@ function range(...nums){
             array.push(i)
         }
 
-        console.log(array)
+        return array
 
     }else if(nums.length == 3 && nums[0] > nums[1]){
         for(i = nums[0]; i > nums[1]; i -= nums[2]){
             array.push(i)
         }
 
-        console.log(array)
+        return array
     }
 }
 
-range(5)
-range(6, 11)
-range(10, 19, 2)
-range(6, 2)
-range(8, -3, 4)
+console.log(range(5))
+console.log(range(6, 11))
+console.log(range(10, 19, 2))
+console.log(range(6, 2))
+console.log(range(8, -3, 4))
