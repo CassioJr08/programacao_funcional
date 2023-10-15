54091e783fe7a63c84074af2ce55545aae07e5b0
CassioJr08 <cassiojr0108@gmail.com>
2023-10-15 14:44:33 -0300

    nova sessao

---
+++ funcional/first-class-function.js
@@ -0,0 +1,18 @@
+let x = 10
+const add = function(a, b){
+    return a + b
+}
+
+const subtract = function(a, b){
+    return a - b
+}
+
+const multiply = (x, y) => x * y
+
+const division = (x, y) => x / y
+
+
+console.log(add(10, 20))
+console.log(subtract(10, 20))
+console.log(multiply(10, 20))
+console.log(division(10, 20))
