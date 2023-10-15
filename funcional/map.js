52c577e98864aac6899068d89cc963d7950872a0
CassioJr08 <cassiojr0108@gmail.com>
2023-10-15 19:55:15 -0300

    funcao map

---
+++ funcional/map.js
@@ -0,0 +1,21 @@
+// map é responsavel por fazer trasnformações em elementos de uma array
+
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const numbersV2 = numbers.map(elemento => elemento * 2)
+
+console.log(numbers, numbersV2)
+
+const students = [
+    {name: 'Jake', score: 6.4},
+    {name: 'Susan', score: 6.4},
+    {name: 'Emma', score: 6.4},
+    {name: 'Peter', score: 9.1}
+]
+
+const getScore = elemento => elemento.score
+
+const result = students.map(getScore).map(Math.floor)
+
+console.log(result)
+
