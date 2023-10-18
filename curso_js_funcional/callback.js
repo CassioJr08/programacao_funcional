8d910a8654e87f82855cb055542816738f2c5299
CassioJr08 <cassiojr0108@gmail.com>
2023-10-18 10:39:38 -0300

    callbacks

--- curso_js_funcional/callback.js
+++ curso_js_funcional/callback.js
@@ -7,13 +7,13 @@ function exibirConteudo(err, conteudo){
     console.log(conteudo.toString())
 }
 
-console.log('Inicio...1')
+console.log('Inicio...')
 fs.readFile(caminho, exibirConteudo)
 fs.readFile(caminho, (err, data) => console.log(data.toString()))
-console.log('Fim...1')
+console.log('Fim...')
 
 
-console.log('Inicio...2')
+console.log('Inicio Sync...')
 const conteudo = fs.readFileSync(caminho)
 console.log(conteudo.toString())
-console.log('Fim...2')
+console.log('Fim Sync...')
